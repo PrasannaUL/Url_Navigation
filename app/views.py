c9233a7c1fd1b0c2cdc026b0346360dd1316c39f
PrasannaUL <prasannareddy15b3@gmail.com>
2023-11-20 21:01:58 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def Ram(request):
+    return render(request,'Ram.html')
+
+def prasanna(request):
+    return render(request,'prasanna.html')
+
+def Ram2(request):
+    return render(request,'Ram2.html')
